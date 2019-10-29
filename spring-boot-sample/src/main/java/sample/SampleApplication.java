23cf0f9b0be141aa5a39deb2325c4de875939c1e
Guillaume APCHAIN <gapchain@oceaneconsulting.com>
2019-10-29 10:44:55 +0100

    Use of data tests in junit mode

--- spring-boot-sample/src/main/java/sample/SampleApplication.java
+++ spring-boot-sample/src/main/java/sample/SampleApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import sample.repository.MyTableRepository;
@@ -34,18 +35,10 @@ public class SampleApplication {
 	 * @return CommandLineRunner
 	 */
 	@Bean
+	@Profile("!test")
 	public CommandLineRunner generateCatalogData() {
 
 		return strings -> {
-			/*this.myTableRepository.save(MyTableModel.builder().code("1").name("OP01").email("test1@op01.fr").creationDate("2016-01-05 02:59:59").address("LILLE").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
-			this.myTableRepository.save(MyTableModel.builder().code("2").name("OP01").email("test1@op02.fr").creationDate("2017-07-14 02:59:59").address("ARRAS").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
-			this.myTableRepository.save(MyTableModel.builder().code("3").name("OP02").email("test1@op02.fr").creationDate("2018-12-17 02:59:59").address("DOUAI").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
-			this.myTableRepository.save(MyTableModel.builder().code("4").name("OP02").email("test1@op02.fr").creationDate("2017-09-02 02:59:59").address("LENS").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
-			this.myTableRepository.save(MyTableModel.builder().code("5").name("OP03").email("test1@op03.fr").creationDate("2017-07-31 02:59:59").address("VALENCIENNES").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
-			this.myTableRepository.save(MyTableModel.builder().code("6").name("OP03").email("test1@op03.fr").creationDate("2018-04-10 02:59:59").address("BETHUNE").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
-			this.myTableRepository.save(MyTableModel.builder().code("7").name("OP03").email("test1@op03.fr").creationDate("2019-02-09 02:59:59").address("HENIN-BEAUMONT").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
-			this.myTableRepository.save(MyTableModel.builder().code("8").name("OP04").email("test1@op04.be").creationDate("2019-04-19 02:59:59").address("TOURNAI").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
-			this.myTableRepository.save(MyTableModel.builder().code("9").name("OP05").email("test1@op05.fr").creationDate("2019-10-23 17:37:12").address("VILLENEUVE D'ASCQ").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));*/
 
 
 			Faker faker = new Faker();
@@ -61,10 +54,6 @@ public class SampleApplication {
 						.creationDate(DateFormatUtils.formatSqlDateToString(faker.date().birthday()))
 						.address(faker.address().city()).build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
 			}
-
-
-
-
 			Thread.sleep(500);
 
 		};
--- spring-boot-sample/src/test/java/sample/repository/MyTableRepositoryTest.java
+++ spring-boot-sample/src/test/java/sample/repository/MyTableRepositoryTest.java
@@ -3,7 +3,9 @@ package sample.repository;
 import date.DateFormatUtils;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,6 +28,7 @@ import java.util.Locale;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @Log4j2
+@TestInstance(value = TestInstance.Lifecycle.PER_CLASS)
 public class MyTableRepositoryTest {
 
 
@@ -47,6 +50,22 @@ public class MyTableRepositoryTest {
     private static final String OP05 = "OP05";
     private static final String TOURNAI = "TOURNAI";
     private static final String CAMBRAI = "CAMBRAI";
+
+    @BeforeAll
+    public void populateDataTests() {
+        this.myTableRepository.deleteAll();
+        this.myTableRepository.save(MyTableModel.builder().code("1").name("OP01").email("test1@op01.fr").creationDate("2016-01-05 02:59:59").address("LILLE").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
+        this.myTableRepository.save(MyTableModel.builder().code("2").name("OP01").email("test1@op02.fr").creationDate("2017-07-14 02:59:59").address("ARRAS").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
+        this.myTableRepository.save(MyTableModel.builder().code("3").name("OP02").email("test1@op02.fr").creationDate("2018-12-17 02:59:59").address("DOUAI").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
+        this.myTableRepository.save(MyTableModel.builder().code("4").name("OP02").email("test1@op02.fr").creationDate("2017-09-02 02:59:59").address("LENS").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
+        this.myTableRepository.save(MyTableModel.builder().code("5").name("OP03").email("test1@op03.fr").creationDate("2017-07-31 02:59:59").address("VALENCIENNES").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
+        this.myTableRepository.save(MyTableModel.builder().code("6").name("OP03").email("test1@op03.fr").creationDate("2018-04-10 02:59:59").address("BETHUNE").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
+        this.myTableRepository.save(MyTableModel.builder().code("7").name("OP03").email("test1@op03.fr").creationDate("2019-02-09 02:59:59").address("HENIN-BEAUMONT").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
+        this.myTableRepository.save(MyTableModel.builder().code("8").name("OP04").email("test1@op04.be").creationDate("2019-04-19 02:59:59").address("TOURNAI").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
+        this.myTableRepository.save(MyTableModel.builder().code("9").name("OP05").email("test1@op05.fr").creationDate("2019-10-23 17:37:12").address("VILLENEUVE D'ASCQ").build()).log().subscribe(item -> log.info("Inserting {}", item.toString()));
+
+    }
+
 
 
     @Test
